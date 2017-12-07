fd9b0d5c48b52801884883aa39ffb19547dc3904
Nguyen Viet Hoa <nguyenviethoa1984@gmail.com>
2017-12-07 13:44:26 +0700

    Update ReactNativeDocumentPicker.java

--- android/src/main/java/com/reactnativedocumentpicker/ReactNativeDocumentPicker.java
+++ android/src/main/java/com/reactnativedocumentpicker/ReactNativeDocumentPicker.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class ReactNativeDocumentPicker implements ReactPackage {
 
-    @Override
+    
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
